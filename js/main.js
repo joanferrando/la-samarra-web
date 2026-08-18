9b958860b2e0957a8f6e805060900930f4c13565
joanferrando <44470063+joanferrando@users.noreply.github.com>
2026-08-18 10:47:51 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const mainNav = document.querySelector('.main-nav');
 
+const navLinks = document.querySelectorAll('.main-nav a');
+
+
+menuToggle.addEventListener('click', () => {
+
+    const isOpen =
+        mainNav.classList.toggle('active');
+
+    menuToggle.setAttribute(
+        'aria-expanded',
+        isOpen
+    );
+
+
+    if (isOpen) {
+
+        menuToggle.innerHTML =
+            '<i class="fa-solid fa-xmark"></i>';
+
+    } else {
+
+        menuToggle.innerHTML =
+            '<i class="fa-solid fa-bars"></i>';
+
+    }
+
+});
+
+
+navLinks.forEach(link => {
+
+    link.addEventListener('click', () => {
+
+        mainNav.classList.remove('active');
+
+        menuToggle.setAttribute(
+            'aria-expanded',
+            'false'
+        );
+
+        menuToggle.innerHTML =
+            '<i class="fa-solid fa-bars"></i>';
+
+    });
+
+});
